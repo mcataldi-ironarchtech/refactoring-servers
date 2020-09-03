c8290f1827a5454470f91411792dc53b3cb6a451
Matthew Cataldi <mcataldi@ironarchtech.com>
2020-09-03 12:57:45 -0500

    Iniital commit of work.  Set up to transfer to other computers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Server } from './shared/server.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'refactoring-app-again';
+  serverElements = [
+    new Server('server', 'Test Server 01', 'Just a test')
+  ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push(
+      new Server('server', serverData.serverName, serverData.serverContent)
+    );
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}){
+    this.serverElements.push(
+      new Server('server', blueprintData.serverName, blueprintData.serverContent)
+    );
+  }
+
 }
