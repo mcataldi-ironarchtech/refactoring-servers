69f3c5c2db2d0a9c5180fbf6d4c3ddf776595933
Matthew Cataldi <mcataldi@ironarchtech.com>
2020-09-09 15:45:51 -0500

    Work on Lifecycle Hooks on the ServerElement component.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+    Component, OnInit, Input, ViewEncapsulation, OnChanges, 
+    SimpleChanges, DoCheck, AfterContentInit, AfterViewInit, 
+    AfterViewChecked, AfterContentChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +9,47 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // Conventional Default
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, 
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  // Best practice to put lifecycle hooks in 'implements' portion and then in the import statement.
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input('srvName') name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called');
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck');
+
+    // Don't run high performant code here because will cost a lot.
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+  }
 }
